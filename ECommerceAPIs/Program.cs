12437d19edd3160ceb3b2f1156264e4f2eecf6b6
Mazen Elnems <mazenelnems22@gmail.com>
2026-03-06 14:53:09 +0200

    Add CORS support with permissive default policy
    Added CORS configuration to allow any origin, method, and header. Integrated CORS middleware into the request pipeline to enable cross-origin requests.

--- ECommerceAPIs/Program.cs
+++ ECommerceAPIs/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -33,6 +43,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
